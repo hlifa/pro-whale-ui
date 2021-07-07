bf82adc96d26e6d9dd0619082047da12004ff5ef
hlifa <hu2014jesse@outlook.com>
2021-07-07 11:05:19 +0800

    fix: modal form visible

--- packages/pro-form/src/modal-form.vue
+++ packages/pro-form/src/modal-form.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: your name
  * @Date: 2021-03-26 15:26:30
- * @LastEditTime: 2021-04-01 20:52:41
+ * @LastEditTime: 2021-07-07 11:04:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /whale-ui/packages/pro-form/src/modal-form.vue
 -->
 
 <template>
-  <el-dialog :visible.sync="visible" v-bind="resolvedElDialogProps">
+  <el-dialog :visible.sync="internalVisible" v-bind="resolvedElDialogProps">
     <pro-base-form
       v-bind="resolvedAttrs"
       v-on="$listeners"
@@ -59,6 +59,17 @@ export default {
         width: "50%",
       },
     };
+  },
+
+  watch: {
+    internalVisible: {
+      get() {
+        return this.visible;
+      },
+      set(val) {
+        this.$emit("update:visible", val);
+      },
+    },
   },
 
   computed: {
